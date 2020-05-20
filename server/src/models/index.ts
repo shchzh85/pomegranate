2d0aceb7ddc00a41a6875861d968bbab85b8039f
xiong <g.wang.x@gmail.com>
2020-05-20 20:06:48 +0800

    fix

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,7 +3,7 @@ import { sequelize } from '@common/dbs';
 
 import { UserModel } from './user.model';
 import { WalletModel } from './wallet.model';
-import { CoinKindModel } from './coinkind.model';
+import { CoinKindModel } from './coin_kind.model';
 
 export const userRepository = sequelize.getRepository<UserModel>(UserModel);
 export const walletModel = sequelize.getRepository<WalletModel>(WalletModel);
