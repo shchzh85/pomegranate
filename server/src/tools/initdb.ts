eac7ee617317dbe13ccb8e0c2c32494fe1ce3171
hermitsgame <g.wang.x@gmail.com>
2020-05-24 18:55:39 +0800

    f

--- server/src/tools/initdb.ts
+++ server/src/tools/initdb.ts
@@ -3,8 +3,27 @@ require('module-alias/register');
 
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
-
 import { sequelize } from '@common/dbs';
+import { userRepository, coinKindRepository } from '@models/index';
 
-sequelize.sync({ force: true })
-.then(() => console.log('done.'));
+async function work() {
+    await sequelize.sync({ force: true });
+
+    await userRepository.create({
+        username: 'root',
+        password: '111111',
+        dpassword: '111111',
+        invitecode: '88888888',
+        pid: 0,
+        tops: ''
+    });
+
+//    await coinKindRepository.bulkCreate([
+
+//   ]);
+}
+
+work()
+.then(() => {
+    console.log('done.');
+});
