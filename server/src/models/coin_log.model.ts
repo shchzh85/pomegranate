b4f2ed660ce7be3f1ba13429a89d8b644393a459
hermits <g.wang.x@gmail.com>
2020-05-27 11:17:56 +0800

    fig bug

--- server/src/models/coin_log.model.ts
+++ server/src/models/coin_log.model.ts
@@ -7,54 +7,48 @@ import {
 } from 'sequelize-typescript';
 
 @Table({
-  tableName: 'tpz_user_wallet',
+  tableName: 'tpz_coin_log',
+  indexes: [
+    { name: 'co_u_x', fields: [ 'uid', 'wtype' ] },
+    { name: 'cod_u_x', fields: [ 'wtype' ] }
+  ]
 })
 export class CoinLogModel extends Model<CoinLogModel> {
 
   @Column({
-    field: 'uid',
     allowNull: false,
     comment: '用户id'
   })
   public uid!: number;
 
   @Column({
-    field: 'targetid',
     allowNull: false,
     comment: '目标用户id'
   })
   public targetid!: number;
 
   @Column({
-    field: 'num',
     type: DataType.DECIMAL(20, 2),
     allowNull: false,
-    defaultValue: 0,
     comment: '变动数量'
   })
   public num!: number;
 
   @Column({
-    field: 'oamount',
     type: DataType.DECIMAL(20, 2),
     allowNull: false,
-    defaultValue: 0,
     comment: '变动前数量'
   })
   public oamount!: number;
 
   @Column({
-    field: 'namount',
     type: DataType.DECIMAL(20, 2),
     allowNull: false,
-    defaultValue: 0,
     comment: '变动后数量'
   })
   public namount!: number;
 
-
   @Column({
-    field: 'username',
     allowNull: false,
     defaultValue: '',
     comment: '用户名称'
@@ -62,67 +56,51 @@ export class CoinLogModel extends Model<CoinLogModel> {
   public username!: string;
 
   @Column({
-    field: 'target',
     allowNull: false,
-    defaultValue: '',
     comment: '目标用户名称'
   })
   public target!: string;
 
   @Column({
-    type: DataType.INTEGER,
+    type: DataType.TINYINT(2),
     allowNull: false,
-    defaultValue: 0,
-    comment: '钱包id'
+    comment: 'coinid'
   })
   public wtype!: number;
 
   @Column({
-    type: DataType.INTEGER,
+    type: DataType.TINYINT(2),
     allowNull: false,
-    defaultValue: 0,
+    defaultValue: 1,
     comment: '变动类型(1增加,2减少)'
   })
   public ntype!: number;
 
   @Column({
-    field: 'note',
     allowNull: false,
-    defaultValue: '',
     comment: '日志类型'
   })
   public note!: string;
 
   @Column({
-    allowNull: true,
-    defaultValue: '',
     comment: '备注补充'
   })
   public remark!: string;
 
   @Column({
-    allowNull: true,
-    defaultValue: '',
+    allowNull: false,
     comment: '日志动作行为'
   })
   public action!: string;
 
   @Column({
-    type: DataType.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
     comment: '行为id'
   })
   public actionid!: number;
 
   @Column({
-    type: DataType.DATE,
     allowNull: false,
-    defaultValue: 0,
     comment: '变动日期'
   })
-  public dtime!: number;
-
-
-
+  public dtime!: Date;
 }
--- server/src/store/coin_log.store.ts
+++ server/src/store/coin_log.store.ts
@@ -1,65 +1,31 @@
 
 import * as _ from 'lodash';
-import { UniqueConstraintError, Op, Transaction } from 'sequelize';
-import { Sequelize } from 'sequelize-typescript';
+import { Transaction } from 'sequelize';
 import BaseStore from './base.store';
-import { coinLogModelRepository } from '@models/index'
-import { Exception } from '@common/exceptions';
-import { ErrCode } from '@common/enums';
-import { sequelize } from '@common/dbs';
-import { md5 } from '@common/utils';
-import { redisStore } from './redis.store';
+import { coinLogRepository } from '@models/index'
 
-export interface CoinLogParams {
-  logArr: {
-    uid: number,
-    username: string,
-    num: number,
-    target: number,
-    targetid: string,
-    wtype: number,
-    ntype: number,
-    oamount: number,
-    namount: number,
-    note: string,
-    dtime: string,
-    action: string,
-    actionid: number
-  }[];
+interface CoinLogParams {
+  uid: number;
+  username: string;
+  num: number;
+  target: string;
+  targetid: number;
+  wtype: number;
+  ntype: number;
+  oamount: number;
+  namount: number;
+  note: string;
+  action: string;
+  actionid: number;
 }
 
 class CoinlogStore extends BaseStore {
 
-
-  public async createLog(params: CoinLogParams) {
-
-    let transaction;
-
-    try {
-      transaction = await sequelize.transaction();
-      const ws = params.logArr.map(v => {
-        return {
-          uid: v.uid,
-          username: v.username,
-          num: v.num,
-          target: v.target,
-          targetid: v.targetid,
-          wtype: v.wtype,
-          ntype: v.ntype,
-          oamount: v.oamount,
-          namount: v.namount,
-          note: v.note,
-          dtime: v.dtime,
-          action: v.action,
-          actionid: v.actionid
-        };
-      });
-
-      await coinLogModelRepository.bulkCreate(ws, { transaction });
-      transaction.commit();
-    } catch (e) {
-      await transaction?.rollback();
-    }
+  public bulkCreate(data: CoinLogParams[], transaction?: Transaction) {
+    const now = new Date();
+    const logs = data.map(v => { return { ...v, dtime: now } });
+    return coinLogRepository.bulkCreate(logs, { transaction });
   }
 }
+
 export const coinLogStroe = new CoinlogStore();
