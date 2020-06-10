dc1e7756b0dc76414ad42768f0e69908ea60c626
xiong <g.wang.x@gmail.com>
2020-06-10 14:23:31 +0800

    f

--- server/src/common/dbs/redis.ts
+++ server/src/common/dbs/redis.ts
@@ -3,6 +3,7 @@ import redis from 'redis';
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST || 'redis',
   port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD
 });
 
 export { redisClient };
